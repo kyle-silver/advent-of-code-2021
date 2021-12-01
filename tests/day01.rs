bd7581692750c5df1d6e529510546b9274bb398f
Kyle Silver <kyleAsilver@gmail.com>
2021-12-01 17:38:09 -0500

    day 1

---
+++ tests/day01.rs
@@ -0,0 +1,20 @@
+const INPUT: &str = include_str!("res/01.txt");
+
+#[test]
+fn part1() {
+    let data: Vec<i32> = INPUT.lines().map(str::parse).map(Result::unwrap).collect();
+    let times_increased = data.windows(2).filter(|pair| pair[1] > pair[0]).count();
+    println!("Day 1, part 1: {}", times_increased);
+    assert_eq!(times_increased, 1559);
+}
+
+#[test]
+fn part2() {
+    let data: Vec<i32> = INPUT.lines().map(str::parse).map(Result::unwrap).collect();
+    let times_increased = data
+        .windows(4)
+        .filter(|sextuplet| sextuplet[1..].iter().sum::<i32>() > sextuplet[..3].iter().sum::<i32>())
+        .count();
+    println!("Day 1, part 2: {}", times_increased);
+    assert_eq!(times_increased, 1600)
+}
