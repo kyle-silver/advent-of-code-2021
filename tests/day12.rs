963496ad75c620d43543071e98faa54854acf30c
Kyle Silver <kyleAsilver@gmail.com>
2022-01-23 13:26:44 -0500

    day 12 part 2: now with 100% more traits!

--- tests/day12.rs
+++ tests/day12.rs
@@ -28,41 +28,109 @@ impl Vertex<'_> {
     }
 }
 
-#[derive(Debug, Default)]
-struct Graph<'a>(HashMap<Vertex<'a>, HashSet<Vertex<'a>>>);
+trait Graph<'a> {
+    fn candidates(&self, current: &Vertex<'a>, visited: &[&Vertex<'a>]) -> Vec<&Vertex<'a>>;
+}
 
-impl<'a> Graph<'a> {
-    fn new(lines: impl Iterator<Item = &'a str>) -> Graph<'a> {
-        let edges: HashSet<_> = lines
-            .map(|token| {
-                let (a, b) = token.split_once('-').unwrap();
-                let (a, b) = (Vertex::new(a), Vertex::new(b));
-                [(a.clone(), b.clone()), (b, a)]
-            })
-            .flat_map(|a| a.into_iter())
-            .collect();
-        let mut vertices: HashMap<_, HashSet<_>> = HashMap::new();
-        for (a, b) in edges {
-            match vertices.entry(a) {
-                Entry::Occupied(mut occupied) => {
-                    occupied.get_mut().insert(b);
-                }
-                Entry::Vacant(vacant) => {
-                    let mut set = HashSet::new();
-                    set.insert(b);
-                    vacant.insert(set);
-                }
+fn parse<'b>(lines: impl Iterator<Item = &'b str>) -> HashMap<Vertex<'b>, HashSet<Vertex<'b>>> {
+    let edges: HashSet<_> = lines
+        .map(|token| {
+            let (a, b) = token.split_once('-').unwrap();
+            let (a, b) = (Vertex::new(a), Vertex::new(b));
+            [(a.clone(), b.clone()), (b, a)]
+        })
+        .flat_map(|a| a.into_iter())
+        .collect();
+    let mut vertices: HashMap<_, HashSet<_>> = HashMap::new();
+    for (a, b) in edges {
+        match vertices.entry(a) {
+            Entry::Occupied(mut occupied) => {
+                occupied.get_mut().insert(b);
+            }
+            Entry::Vacant(vacant) => {
+                let mut set = HashSet::new();
+                set.insert(b);
+                vacant.insert(set);
             }
         }
-        Graph(vertices)
     }
+    vertices
+}
 
+#[derive(Debug)]
+struct SimpleGraph<'a>(HashMap<Vertex<'a>, HashSet<Vertex<'a>>>);
+
+impl<'a> SimpleGraph<'a> {
+    fn new(lines: impl Iterator<Item = &'a str>) -> Self {
+        let vertices = parse(lines);
+        Self(vertices)
+    }
+}
+
+impl<'a> Graph<'a> for SimpleGraph<'a> {
     fn candidates(&self, current: &Vertex<'a>, visited: &[&Vertex<'a>]) -> Vec<&Vertex<'a>> {
         self.0
             .get(current)
             .unwrap()
             .iter()
             .filter(|n| n.big() || !visited.contains(n))
+            .collect()
+    }
+}
+
+#[derive(Debug)]
+struct MultiVisitGraph<'a>(HashMap<Vertex<'a>, HashSet<Vertex<'a>>>);
+
+impl<'a> MultiVisitGraph<'a> {
+    fn new(lines: impl Iterator<Item = &'a str>) -> Self {
+        let vertices = parse(lines);
+        Self(vertices)
+    }
+}
+
+impl<'a> Graph<'a> for MultiVisitGraph<'a> {
+    /// for a `MultiVisitGraph`, a single small cave can be visited twice. All
+    /// other small caves can be visited at most once.
+    fn candidates(&self, current: &Vertex<'a>, visited: &[&Vertex<'a>]) -> Vec<&Vertex<'a>> {
+        // count the number of times each vertex was visited
+        let mut freqs: HashMap<_, u32> = HashMap::new();
+        for vertex in visited {
+            match freqs.entry(vertex) {
+                Entry::Occupied(mut occupied) => {
+                    *occupied.get_mut() += 1;
+                }
+                Entry::Vacant(vacant) => {
+                    vacant.insert(1);
+                }
+            }
+        }
+        // find the one vertex that was potentially visited twice
+        let twice = freqs
+            .iter()
+            .any(|(v, t)| matches!(v, Vertex::Small(_)) && *t >= 2);
+        // if we have already visited a node twice and `current` is being
+        // visited for a second time, we are on an invalid path.
+        if twice && visited.contains(&current) {
+            if let Vertex::Small(_) = current {
+                return Vec::new();
+            }
+        }
+        self.0
+            .get(current)
+            .unwrap()
+            .iter()
+            .filter(|v| match freqs.get(v) {
+                Some(&times_visited) => match v {
+                    Vertex::Start => false,
+                    Vertex::End => true,
+                    Vertex::Small(_) => match twice {
+                        true => times_visited < 1,
+                        false => times_visited < 2,
+                    },
+                    Vertex::Big(_) => true,
+                },
+                None => true,
+            })
             .collect()
     }
 }
@@ -74,14 +142,13 @@ impl<'a> Graph<'a> {
 struct Node<'v, 'id> {
     vertex: &'v Vertex<'id>,
     visited: Vec<&'v Vertex<'id>>,
-    children: Vec<Box<Node<'v, 'id>>>,
 }
 
 impl<'v, 'id> Node<'v, 'id> {
     fn compute(
         vertex: &'v Vertex<'id>,
         parent: Option<&Node<'v, 'id>>,
-        graph: &'v Graph<'id>,
+        graph: &'v impl Graph<'id>,
     ) -> (Node<'v, 'id>, u32) {
         // the "path" for this node is the path of the parent plus the parent
         // itself. (or an empty path if no parent)
@@ -94,11 +161,7 @@ impl<'v, 'id> Node<'v, 'id> {
             None => Vec::new(),
         };
         // construct the node, but without the children computed
-        let mut node = Node {
-            vertex,
-            visited,
-            children: Vec::new(),
-        };
+        let node = Node { vertex, visited };
         // if this is a terminal node, short circuit
         if let Vertex::End = node.vertex {
             return (node, 1);
@@ -111,8 +174,7 @@ impl<'v, 'id> Node<'v, 'id> {
         let mut endpoints = 0;
         // compute the children by acting recursively on the candidates
         for candidate in candidates {
-            let (child, child_endpoints) = Node::compute(candidate, Some(&node), graph);
-            node.children.push(Box::new(child));
+            let (_, child_endpoints) = Node::compute(candidate, Some(&node), graph);
             endpoints += child_endpoints;
         }
         return (node, endpoints);
@@ -121,8 +183,16 @@ impl<'v, 'id> Node<'v, 'id> {
 
 #[test]
 fn part1() {
-    let graph = Graph::new(INPUT.lines());
+    let graph = SimpleGraph::new(INPUT.lines());
     let (_, result) = Node::compute(&Vertex::Start, None, &graph);
     println!("Day 12, part 1: {}", result);
     assert_eq!(4754, result);
+}
+
+#[test]
+fn part2() {
+    let graph = MultiVisitGraph::new(INPUT.lines());
+    let (_, result) = Node::compute(&Vertex::Start, None, &graph);
+    println!("Day 12, part 2: {}", result);
+    assert_eq!(143562, result);
 }
